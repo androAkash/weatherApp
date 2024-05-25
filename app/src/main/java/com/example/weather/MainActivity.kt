8c23c98c83adf87c66c2ea3802205a8ffe84263c
Akash <akashbhattacharyak1314@gmail.com>
2024-05-25 09:04:43 +0530

    Weekend practice has been started

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.weather.ui.theme.WeatherTheme
+
+//25/05/2024 Akash Bhattacharya's Weather app
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+
+            }
+        }
+    }
+}
